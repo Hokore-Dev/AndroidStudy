6f684384a718b45231e426044b90336e8231ba6f
minjun.ha <minjun.ha@sundaytoz.com>
2018-06-18 18:14:29 +0900

    Intent 공부 추가

---
+++ Intent/app/src/main/java/com/example/study/intent/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.study.intent
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            var secondActivity = Intent(this, SecondActivity::class.java)
+            startActivity(secondActivity)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPuase")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop")
+    }
+}
