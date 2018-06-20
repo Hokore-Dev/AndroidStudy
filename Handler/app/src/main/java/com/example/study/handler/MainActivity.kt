20f73e344521e7126af3f28d3069c046be16a710
minjun.ha <minjun.ha@sundaytoz.com>
2018-06-20 14:48:37 +0900

    Handler 공부 추가

---
+++ Handler/app/src/main/java/com/example/study/handler/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.study.handler
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var thread = Thread {
+            run {
+                Thread.sleep(3000)
+                Handler(Looper.getMainLooper()).post {
+                    textview_main.setText("쓰레드 작동")
+                }
+            }
+        }
+        thread.start()
+    }
+}
